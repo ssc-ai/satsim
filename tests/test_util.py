1011be6e13b8bfd915b3f7643c4f8997743aaf67
mixxen <alexander.cabello@gmail.com>
2024-05-29 08:00:18 +0000

    Add merge_dicts tests.

--- tests/test_util.py
+++ tests/test_util.py
@@ -9,6 +9,7 @@ import numpy as np
 from satsim.util.timer import tic, toc, get_timing
 from satsim.util.thread import ThreadedTaskQueue, MultithreadedTaskQueue
 from satsim.util.system import get_semantic_version
+from satsim.util.python import merge_dicts
 
 
 def test_tic_toc():
@@ -109,6 +110,54 @@ def test_get_semantic_version():
     ver = get_semantic_version(t)
 
     assert np.array_equal(ver, [2,1,0])
+
+
+def test_merge_basic():
+    d1 = {'a': 1, 'b': {'x': 10}}
+    d2 = {'b': {'y': 20}, 'c': 3}
+    expected = {'a': 1, 'b': {'x': 10, 'y': 20}, 'c': 3}
+    merge_dicts(d1, d2)
+    assert d1 == expected
+
+
+def test_merge_with_overwrite():
+    d1 = {'a': 1, 'b': {'x': 10}}
+    d2 = {'b': {'x': 20}, 'c': 3}
+    expected = {'a': 1, 'b': {'x': 20}, 'c': 3}
+    merge_dicts(d1, d2)
+    assert d1 == expected
+
+
+def test_merge_with_nested_dict():
+    d1 = {'a': 1, 'b': {'x': {'y': 10}}}
+    d2 = {'b': {'x': {'z': 20}}, 'c': 3}
+    expected = {'a': 1, 'b': {'x': {'y': 10, 'z': 20}}, 'c': 3}
+    merge_dicts(d1, d2)
+    assert d1 == expected
+
+
+def test_merge_with_non_dict_value():
+    d1 = {'a': 1, 'b': {'x': 10}}
+    d2 = {'b': 'new_value', 'c': 3}
+    expected = {'a': 1, 'b': 'new_value', 'c': 3}
+    merge_dicts(d1, d2)
+    assert d1 == expected
+
+
+def test_merge_empty_dict():
+    d1 = {'a': 1, 'b': {'x': 10}}
+    d2 = {}
+    expected = {'a': 1, 'b': {'x': 10}}
+    merge_dicts(d1, d2)
+    assert d1 == expected
+
+
+def test_merge_into_empty_dict():
+    d1 = {}
+    d2 = {'a': 1, 'b': {'x': 10}}
+    expected = {'a': 1, 'b': {'x': 10}}
+    merge_dicts(d1, d2)
+    assert d1 == expected
 
 # cannot run these more than once
 # def test_configure_eager():
