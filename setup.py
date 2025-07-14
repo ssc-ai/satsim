52c8fea47d83cb295b86d66f7906c68a0573fb2c
mixxen <alexander.cabello@gmail.com>
2025-07-14 19:43:05 +0000

    Bump version to 0.20.4 and update related files

--- setup.py
+++ setup.py
@@ -70,6 +70,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ssc-ai/satsim',
-    version='0.20.3',
+    version='0.20.4',
     zip_safe=False,
 )
